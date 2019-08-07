6546fca87a246dd155201112b5d3c5319ff85bb3
John Tatum <jtatum@iMac.local>
2019-08-07 09:44:42 -0400

    Accel fix

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -372,8 +372,8 @@ class CarInterface(object):
        (ret.brakePressed and (not self.brake_pressed_prev or ret.vEgo > 0.001)):
       events.append(create_event('pedalPressed', [ET.NO_ENTRY, ET.USER_DISABLE]))
 
-    if ret.gasPressed:
-      events.append(create_event('pedalPressed', [ET.PRE_ENABLE]))
+    #if ret.gasPressed:
+     # events.append(create_event('pedalPressed', [ET.PRE_ENABLE]))
 
     ret.events = events
 
